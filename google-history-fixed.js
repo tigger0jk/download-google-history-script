8741e82e973287b6d234fff5d13a4a54e8fd1208
Patrick Fay <pfay.us@gmail.com>
2013-11-16 21:46:48 -0800

    testing

--- google-history-fixed.js
+++ google-history-fixed.js
@@ -928,6 +928,7 @@ Object.prototype.sort = function(desc) {
 }
 
 // Let's make sure we're in the right place first
+alert(document.location.href.match);
 if (document.location.href.match(/https:\/\/(history\.)?google\.com\/history/) != null) {
 	// First load SWFObject, necessary for Downloadify
 	/* SWFObject v2.1 <http://code.google.com/p/swfobject/>
